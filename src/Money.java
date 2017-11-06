3b767d953b1b7129340ef1571162def7c8754b52
PiotrCzyz <sabath91@gmail.com>
2017-11-07 00:14:29 +0100

    chapter 10 & 11

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Money {
+
+    protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount && currency.equals(money.currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+
 }
